eabb838817fb1402d1e192613ce23432ea3f0447
junsee1412 <dat10319@gmail.com>
2022-12-13 14:39:08 +0700

    sideBar function

---
+++ server.d/sidebar.dataset.r
@@ -0,0 +1,3 @@
+datasets = list.files(path = "./dataset", pattern = NULL, all.files = FALSE,
+           full.names = FALSE, recursive = FALSE,
+           ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
--- server.r
+++ server.r
@@ -1,3 +1,12 @@
+source("server.d/sidebar.dataset.r")
+source("server.d/sidebar.working.r")
+
 server = function(input, output, session) {
-  
+    observe({
+        updateSelectInput(session,
+            "dataset",
+            choices = datasets,
+            selected = datasets[1]
+        )
+    })
 }
